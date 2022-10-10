9e1749a525c2bb8feef0ba9464aaf1d4361836c6
Nitesh Kumar <niteshchandra039@gmail.com>
2022-10-10 22:25:37 +0530

    modified:   models/tfjs_Iband/model.json 	modified:   private/prediction.js

--- private/prediction.js
+++ private/prediction.js
@@ -1,12 +1,14 @@
 // JavaScript
 
-// import * as tf from '@tensorflow/tfjs';
-const tf = require('@tensorflow/tfjs');
-require('@tensorflow/tfjs-node');
+import * as tf from '@tensorflow/tfjs';
 
-const model = tf.loadLayersModel('file://home/astrolab/firebase/models/tfjs_Iband/model.json');
+// const model = tf.loadLayersModel('file://home/astrolab/firebase/models/tfjs_Iband/model.json');
+
+const model = await tf.loadLayersModel("http://localhost:5500/models/tfjs_Iband/model.json");
 
 const example = [0.5, 1.4, 5, 5, 5, 5];  // for example
+
+// example.dtype = 'float64';
 
 const prediction = model.predict(tf.tensor2d([example], [1, 6]));
 
